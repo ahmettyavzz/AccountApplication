25e0e564a0a2b0acbb0066ee5a3cd75d2df120c8
ahmettyavzz <99591875+ahmettyavzz@users.noreply.github.com>
2022-12-29 20:50:43 +0300

    Initial commit

---
+++ src/main/java/com/example/folksdevspringboot/FolksDevSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.folksdevspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FolksDevSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FolksDevSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/folksdevspringboot/FolksDevSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.folksdevspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FolksDevSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
