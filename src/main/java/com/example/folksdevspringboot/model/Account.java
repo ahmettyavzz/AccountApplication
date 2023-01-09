370602f1a5fc993de6455a71dd8680eb7d07557a
ahmettyavzz <99591875+ahmettyavzz@users.noreply.github.com>
2023-01-10 00:26:49 +0300

    create constructor in Account

--- src/main/java/com/example/folksdevspringboot/model/Account.java
+++ src/main/java/com/example/folksdevspringboot/model/Account.java
@@ -1,6 +1,8 @@
 package com.example.folksdevspringboot.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.CascadeType;
@@ -11,26 +13,34 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import java.time.LocalDateTime;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Account {
 
     @javax.persistence.Id
     @GeneratedValue(generator = "UUID")
     @GenericGenerator(
             name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
-    private String id;
+    private String id = "";
     private Long balance;
     private LocalDateTime creationDate;
 
-    @ManyToOne(fetch = FetchType.LAZY,cascade = CascadeType.ALL)
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "customer_id", nullable = false)
     private Customer customer;
 
-    @OneToMany(mappedBy = "account", fetch = FetchType.LAZY)
-    private Set<Transaction> transaction;
+    @OneToMany(mappedBy = "account", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    private List<Transaction> transactions= new ArrayList<>();
 
+    public Account(Long initialCredit, LocalDateTime now, Customer customer) {
+        this.balance = initialCredit;
+        this.creationDate = now;
+        this.customer = customer;
+    }
 
 }
