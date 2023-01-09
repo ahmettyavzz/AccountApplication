885ec725861dcce87c2673510c64546ebccab665
ahmettyavzz <99591875+ahmettyavzz@users.noreply.github.com>
2023-01-10 00:46:15 +0300

    create CustomerDtoConverter

--- src/main/java/com/example/folksdevspringboot/converter/CustomerDtoConverter.java
+++ src/main/java/com/example/folksdevspringboot/converter/CustomerDtoConverter.java
@@ -1,11 +1,18 @@
 package com.example.folksdevspringboot.converter;
 
 import com.example.folksdevspringboot.dto.AccountCustomerDto;
+import com.example.folksdevspringboot.dto.CustomerDto;
 import com.example.folksdevspringboot.model.Customer;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import java.util.stream.Collectors;
+
 @Component
+@RequiredArgsConstructor
 public class CustomerDtoConverter {
+
+    private final CustomerAccountDtoConverter converter;
 
     public AccountCustomerDto convertToAccountCustomer(Customer entity) {
 
@@ -13,5 +20,14 @@ public class CustomerDtoConverter {
             return new AccountCustomerDto("", "", "");
         }
         return new AccountCustomerDto(entity.getId(), entity.getName(), entity.getSurname());
+    }
+
+    public CustomerDto convertToCustomerDto(Customer customer) {
+        return new CustomerDto(customer.getId()
+                , customer.getName()
+                , customer.getSurname()
+                , customer.getAccounts().stream().map(converter::convert).collect(Collectors.toList()));
+
+
     }
 }
