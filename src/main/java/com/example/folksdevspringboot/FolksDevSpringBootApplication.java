4356251bf790c56a6df3eea3ce16c81a57cd4946
ahmettyavzz <99591875+ahmettyavzz@users.noreply.github.com>
2023-01-10 00:48:10 +0300

    create Customer in Main for Test

--- src/main/java/com/example/folksdevspringboot/FolksDevSpringBootApplication.java
+++ src/main/java/com/example/folksdevspringboot/FolksDevSpringBootApplication.java
@@ -1,13 +1,29 @@
 package com.example.folksdevspringboot;
 
+import com.example.folksdevspringboot.model.Customer;
+import com.example.folksdevspringboot.repository.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+
 @SpringBootApplication
-public class FolksDevSpringBootApplication {
+public class FolksDevSpringBootApplication implements CommandLineRunner {
+
+    private final CustomerRepository customerRepository;
+
+    public FolksDevSpringBootApplication(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(FolksDevSpringBootApplication.class, args);
     }
 
+    @Override
+    public void run(String... args){
+        Customer customer = customerRepository.save(new Customer("", "ahmet", "yavuz", new ArrayList<>()));
+        System.out.println(customer);
+    }
 }
