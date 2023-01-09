d9efc6f6ec322fdea1c22a60a0eab069d04c74a5
ahmettyavzz <99591875+ahmettyavzz@users.noreply.github.com>
2023-01-10 00:30:39 +0300

    create constructor in Transaction

--- src/main/java/com/example/folksdevspringboot/model/Transaction.java
+++ src/main/java/com/example/folksdevspringboot/model/Transaction.java
@@ -1,10 +1,11 @@
 package com.example.folksdevspringboot.model;
 
 import com.example.folksdevspringboot.model.type.TransactionType;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.GenericGenerator;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
@@ -14,6 +15,8 @@ import java.time.LocalDateTime;
 
 @Entity
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Transaction {
     @javax.persistence.Id
     @GeneratedValue(generator = "UUID")
@@ -24,7 +27,14 @@ public class Transaction {
     private Long amount;
     private LocalDateTime transactionDate;
 
-    @ManyToOne(fetch = FetchType.LAZY,optional = false,cascade = CascadeType.ALL)
-    @JoinColumn(name = "account_id",nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "account_id", nullable = false)
     private Account account;
+
+    public Transaction(Long amount, Account account) {
+        this.transactionType = TransactionType.INITIAL;
+        this.amount = amount;
+        this.transactionDate = LocalDateTime.now();
+        this.account = account;
+    }
 }
