7b9515c0bb53a7173f928d1c60f1565b540ef555
ahmettyavzz <99591875+ahmettyavzz@users.noreply.github.com>
2023-01-10 00:39:01 +0300

    call CustomerDtoConverter to CustomerService

--- src/main/java/com/example/folksdevspringboot/service/CustomerService.java
+++ src/main/java/com/example/folksdevspringboot/service/CustomerService.java
@@ -1,5 +1,7 @@
 package com.example.folksdevspringboot.service;
 
+import com.example.folksdevspringboot.converter.CustomerDtoConverter;
+import com.example.folksdevspringboot.dto.CustomerDto;
 import com.example.folksdevspringboot.exception.NotFoundException;
 import com.example.folksdevspringboot.model.Customer;
 import com.example.folksdevspringboot.repository.CustomerRepository;
@@ -10,10 +12,13 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class CustomerService {
     private final CustomerRepository customerRepository;
-
+    private final CustomerDtoConverter customerDtoConverter;
     protected Customer findCustomerById(String id) {
         return customerRepository.findById(id).orElseThrow(() -> new NotFoundException("Customer could not find by id: " + id));
     }
 
 
+    public CustomerDto getCustomerById(String customerId) {
+        return customerDtoConverter.convertToCustomerDto(findCustomerById(customerId));
+    }
 }
